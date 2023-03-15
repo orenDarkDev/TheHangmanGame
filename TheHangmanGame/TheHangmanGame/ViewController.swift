f5fbc3554dc3f40b14fd99bbc90695b63f22fe1c
Oren Pelosof <orenpelosof@gmail.com>
2023-03-15 16:37:04 -0300

    Initial commit.

---
+++ TheHangmanGame/TheHangmanGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheHangmanGame
+//
+//  Created by Oren Pelosof on 15/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

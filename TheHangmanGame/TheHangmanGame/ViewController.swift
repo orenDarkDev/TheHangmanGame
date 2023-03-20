e836c2fec454fdde51477a030c7893368ace2ca1
Oren Pelosof <orenpelosof@gmail.com>
2023-03-20 14:39:19 -0300

    First letterLabel in the lettersView

--- TheHangmanGame/TheHangmanGame/ViewController.swift
+++ TheHangmanGame/TheHangmanGame/ViewController.swift
@@ -21,28 +21,34 @@ class ViewController: UIViewController {
         lettersView.layer.borderColor = UIColor.black.cgColor
         view.addSubview(lettersView)
         
+        let height = 150
+        let width = 80
+        
+        
+            var lettersLabel = UILabel()
+            lettersLabel.translatesAutoresizingMaskIntoConstraints = false
+            lettersLabel.backgroundColor = .white
+            lettersLabel.font = UIFont.systemFont(ofSize: 18)
+            lettersLabel.textAlignment = .center
+            lettersLabel.text = "?"
+            lettersView.addSubview(lettersLabel)
+            
+            
+        
         NSLayoutConstraint.activate([
             lettersView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
             lettersView.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor),
             lettersView.heightAnchor.constraint(equalToConstant: 90),
-            lettersView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            lettersView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            lettersLabel.heightAnchor.constraint(equalToConstant: 70),
+            lettersLabel.widthAnchor.constraint(equalToConstant: 50),
+            lettersLabel.leadingAnchor.constraint(equalTo: lettersView.leadingAnchor, constant: 10),
+            lettersLabel.centerYAnchor.constraint(equalTo: lettersView.centerYAnchor)
+        
         ])
         
-        
-        let height = 150
-        let width = 80
-        
-        for  letters in 0..<numberOfLetters {
-            var lettersLabel = UILabel()
-            lettersLabel.translatesAutoresizingMaskIntoConstraints = false
-            lettersLabel.font = UIFont.systemFont(ofSize: 18)
-            lettersLabel.textAlignment = .center
-            lettersLabel.text = ""
-            
-            
-            
-        }
-    }
+   }
 
     override func viewDidLoad() {
         super.viewDidLoad()

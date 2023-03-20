7a15a7e128ebfb95a63c5339238525d73ff9f3dc
Oren Pelosof <orenpelosof@gmail.com>
2023-03-20 13:22:40 -0300

    In loadView() the UIView lettersView was created and put into place using NSLayoutConstraint.activate([]). Counting letters loop still incomplete

--- TheHangmanGame/TheHangmanGame/ViewController.swift
+++ TheHangmanGame/TheHangmanGame/ViewController.swift
@@ -8,6 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var numberOfLetters: Int = 1
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .orange
+        
+        let lettersView = UIView()
+        lettersView.translatesAutoresizingMaskIntoConstraints = false
+        lettersView.backgroundColor = .lightGray
+        lettersView.layer.borderWidth = 1
+        lettersView.layer.borderColor = UIColor.black.cgColor
+        view.addSubview(lettersView)
+        
+        NSLayoutConstraint.activate([
+            lettersView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            lettersView.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor),
+            lettersView.heightAnchor.constraint(equalToConstant: 90),
+            lettersView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        
+        let height = 150
+        let width = 80
+        
+        for  letters in 0..<numberOfLetters {
+            var lettersLabel = UILabel()
+            lettersLabel.translatesAutoresizingMaskIntoConstraints = false
+            lettersLabel.font = UIFont.systemFont(ofSize: 18)
+            lettersLabel.textAlignment = .center
+            lettersLabel.text = ""
+            
+            
+            
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
